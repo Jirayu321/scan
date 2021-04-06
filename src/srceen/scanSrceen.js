1828edff4eae38bf28c3f4b5bc59d8ea73652584
Your Name <jyung169@gmail.com>
2021-04-06 11:32:02 +0700

    .

--- src/srceen/scanSrceen.js
+++ src/srceen/scanSrceen.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, {useState} from 'react';
-import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
+import {Alert, Modal, StyleSheet, Text, Pressable, View, Image} from 'react-native';
 // import {Data} from './dataSrceen';
+
 
 const scanSrceen = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -16,17 +18,34 @@ const scanSrceen = () => {
           Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
         }}>
+
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             {/* <Data/> */}
-            <View>
-              <Text>รายละเอียด</Text>
+            <View style={styles.container}>
+
+              <View style={styles.Incontainer}>
+                <Image
+                   style={styles.icon}
+                   source={require('../asset/contract.png')}
+                />
+              </View>
+
+              <View style={styles.Incontainer}>
+                  <Text  style={{fontSize:30}}>รายละเอียด</Text>
+              </View>
+
+              <View style={[styles.Incontainer,{ alignSelf: 'flex-start'}]}>
+                <Pressable
+                    style={[ styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={styles.textStyle}>ปิด</Text>
+                </Pressable>
+              </View>
             </View>
-            <Pressable
-              style={[ styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>ปิด</Text>
-            </Pressable>
+            <View>
+              <Text>555555</Text>
+            </View>
           </View>
         </View>
       </Modal>
@@ -34,10 +53,11 @@ const scanSrceen = () => {
         style={[styles.scan, styles.buttonOpen]}
         onPress={() => setModalVisible(true)}>
           <View>
-            <Text  style={[styles.modalInText,{marginTop:100}]}>กรุณาแตะ</Text>
-            <Text  style={[styles.modalInText]}>เพื่อเช็ดยอดเงินคงเหลือ</Text>
+            <Text style={[styles.modalInText,{ marginTop:100 }]}>กรุณาแตะ</Text>
+            <Text style={[styles.modalInText]}>เพื่อเช็ดยอดเงินคงเหลือ</Text>
           </View>
       </Pressable>
+
     </View>
   );
 };
@@ -49,11 +69,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
   },
+  container:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  Incontainer:{
+    margin:10,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   modalView: {
     margin: 20,
     backgroundColor: 'white',
     padding: 35,
-    alignItems: 'center',
     shadowColor: '#000',
     height: 500,
     width: 800,
@@ -75,6 +106,7 @@ const styles = StyleSheet.create({
   },
   buttonClose: {
     backgroundColor: '#2196F3',
+    borderRadius:6,
   },
   textStyle: {
     color: 'white',
@@ -85,6 +117,10 @@ const styles = StyleSheet.create({
     margin: 30,
     textAlign: 'center',
     fontSize: 36,
+  },
+  icon:{
+    width: 50,
+    height: 50,
   },
 });
 export default scanSrceen;
